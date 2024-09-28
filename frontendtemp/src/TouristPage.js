dcaaf9fec6a33e95406b42e8c1a8b03c9d3c4ddf
MahmoudEmara12 <mahmoudemara.411@gmail.com>
2024-09-29 02:27:26 +0300

    BackEnd And FrontEnd LatestUpdate

---
+++ frontendtemp/src/TouristPage.js
@@ -0,0 +1,13 @@
+// TouristPage.js
+import React from 'react';
+
+const TouristPage = () => {
+  return (
+    <div>
+      <h1>Welcome, Tourist!</h1>
+      {/* Additional content goes here */}
+    </div>
+  );
+};
+
+export default TouristPage;
